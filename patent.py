cbd7f8cb62698ce09504dd84ede33506f7895daa
Jerome Cheng <jerome@ayulin.net>
2014-03-24 13:56:19 +0800

    Add factory method signature.

--- patent.py
+++ patent.py
@@ -1,4 +1,13 @@
 class Patent(object):
     """Represents an entire Patent with all its fields."""
 
+    def __init__(self):
+        pass
+
+    def from_file(file_path):
+        """
+        Given the path to a file containing a patent
+        in XML form, returns a Patent object representing
+        the patent within that file.
+        """
 
