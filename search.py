43858a0cd4e31e22046d2c31ce336408029c6092
= <wrong92@gmail.com>
2014-03-24 16:31:11 +0800

    Add search_patents.py

--- search.py
+++ search.py
@@ -1,24 +1,25 @@
 #!/env/bin/python
 import os
 import argparse
+import search_patents
 
 
 def main(args):
     args.dictionary = os.path.abspath(args.dictionary)
     args.postings = os.path.abspath(args.postings)
-    args.queries = os.path.abspath(args.queries)
+    args.query = os.path.abspath(args.query)
     args.output = os.path.abspath(args.output)
+    search_patents.search(args.dictionary, args.postings, args.query,
+                          args.output)
 
-    # TODO:
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='boolean retrieval \
-                                     assignment.')
+    parser = argparse.ArgumentParser(description='Patsnap assignment.')
     parser.add_argument('-d', '--dictionary', type=str,
                         help='dictionary file.', default='dictionary.txt')
     parser.add_argument('-p', '--postings', type=str, help='postings file.',
                         default='postings.txt')
-    parser.add_argument('-q', '--queries', type=str, help='queries file.',
+    parser.add_argument('-q', '--query', type=str, help='query file.',
                         required=True)
     parser.add_argument('-o', '--output', type=str, help='output file.',
                         default='results.txt')
---
+++ search_patents.py
@@ -0,0 +1,11 @@
+def search(dictionary_file, postings_file, query_file, output_file):
+    print dictionary_file
+    print postings_file
+    print query_file
+    print output_file
+
+    # Read and build index files
+
+    # Read and parse query_file
+
+    #
