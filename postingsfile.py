11f807319aafa6f0d9bb3a56fe8f89d2bacaf65d
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-24 23:44:48 +0800

    Extended postings file to accept docid strings.

--- postingsfile.py
+++ postingsfile.py
@@ -25,7 +25,10 @@ class PostingsFile(object):
     Fields are big-endian.
 
     The following record types are supported:
-    - Arrays of (doc_id, term_freq)-tuples
+    - DWords
+    - Length-prefixed strings
+    - Arrays of (doc_id, term_freq)-tuples, where type(doc_id) == str and
+      type(term_freq) == int
     """
     DWORD_SIZE = 4
     DWORD_FMT = '>I'
@@ -36,8 +39,6 @@ class PostingsFile(object):
         mode.
         """
         assert type(filename) == str
-        assert type(filemode) == str
-
         self.__filename = filename
         self.__file_mode = 'rb'
         if write:
@@ -75,17 +76,13 @@ class PostingsFile(object):
         Writes an array of (doc_id, term_frequency)-tuples.
         """
         assert array and type(array) == list
-
         self.__fd.seek(0, os.SEEK_END)
         position = self.__fd.tell()
-
         self.__write_prefix(len(array))
         for item in array:
             assert type(item) == tuple
             assert len(item) == 2
-
             self.__write_idtf_tuple(item)
-
         return position
 
     @requires_open
@@ -94,21 +91,19 @@ class PostingsFile(object):
         Reads an array of (doc_id, term_frequency)-tuples.
         """
         assert type(offset) == int and offset >= 0
-
         result = list()
         self.__fd.seek(offset)
-
         count = self.__read_prefix()
         for i in range(count):
             result.append(self.__read_idtf_tuple())
-
         return result
 
 # Private methods for reading/writing basic data types
 ###############################################################################
     @requires_open
     def __write_DWord(self, dword):
-        byte_array = struct.pack(PostingsFile.DWORD_FMT, int(dword))
+        assert type(dword) == int
+        byte_array = struct.pack(PostingsFile.DWORD_FMT, dword)
         self.__fd.write(byte_array)
         return self.__fd.tell()
 
@@ -116,7 +111,20 @@ class PostingsFile(object):
     def __read_DWord(self):
         byte_array = self.__fd.read(PostingsFile.DWORD_SIZE)
         result = struct.unpack(PostingsFile.DWORD_FMT, byte_array)[0]
+        return result
 
+    @requires_open
+    def __write_string(self, string):
+        assert type(string) == str
+        length = len(string)
+        self.__write_prefix(length)
+        self.__fd.write(string)
+        return self.__fd.tell()
+
+    @requires_open
+    def __read_string(self):
+        length = self.__read_DWord()
+        result = str(self.__fd.read(length))
         return result
 
     @requires_open
@@ -128,7 +136,6 @@ class PostingsFile(object):
     def __read_prefix(self):
         length = self.__read_DWord()
         assert length > 0
-
         return length
 
 # Private methods for reading/writing compound data types
@@ -136,15 +143,11 @@ class PostingsFile(object):
     @requires_open
     def __write_idtf_tuple(self, _tuple):
         assert len(_tuple) == 2
-
         cur = self.__fd.tell()
-        self.__write_DWord(_tuple[0])
+        self.__write_string(_tuple[0])
         effect = self.__write_DWord(_tuple[1])
-
-        assert effect - cur == 2 * PostingsFile.DWORD_SIZE
 
     @requires_open
     def __read_idtf_tuple(self):
-        result = (self.__read_DWord(), self.__read_DWord())
-
+        result = (self.__read_string(), self.__read_DWord())
         return result
