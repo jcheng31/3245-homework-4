9af9d2e125b3e2b2e59da481c2b7406d56d8187d
Michael Yong <wrong92@gmail.com>
2014-03-27 01:07:32 +0800

    Load dict into memory.

--- src/search_patents.py
+++ src/search_patents.py
@@ -1,4 +1,5 @@
 import utils
+import json
 
 
 def search(dictionary_file, postings_file, query_file, output_file):
@@ -8,6 +9,8 @@ def search(dictionary_file, postings_file, query_file, output_file):
     print output_file
 
     # Read and build index files
+    with open(dictionary_file, 'r') as dfile:
+        dictionary = json.loads(dfile.read())
 
     # Read and parse query_file
-    print utils.parse_query_file(query_file)
+    query = utils.parse_query_file(query_file)
